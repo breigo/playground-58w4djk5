4a294ee01e120c13c4db237ee730e356db475577
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2022-02-10 21:17:29 +0100

    Add people example output

---
+++ csharp-project/FilterPeopleWithWhereAndTakeTest.cs
@@ -0,0 +1,39 @@
+﻿using Answer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace TechIo
+{
+    [TestClass]
+    public class FilterPeopleWithWhereAndTakeTest
+    {
+		private bool shouldShowHint = false;
+
+		[TestMethod]
+		public void Verify() 
+		{
+			shouldShowHint = true;
+			TechIO.PrintMessage("Input", People.Instance);
+			var expected = People.Instance.Where(x => x.Age >= 30).Take(2).ToList();
+			var actual = FilterPeopleWithWhereAndTake.TakeFirstTwoAdults(People.Instance).ToList();
+			TechIO.PrintMessage("Output", actual);
+			
+			CollectionAssert.AreEqual(expected, actual);
+			shouldShowHint = false;
+		}
+
+		[TestCleanup()]
+		public void Cleanup()
+		{
+			if(shouldShowHint)
+			{	
+				// On Failure
+				TechIO.PrintMessage("Hint 💡", "Did you properly implemented the method? 🤔");
+			} 
+			else
+			{
+				TechIO.PrintMessage("Kudos 🌟", "You can query objects!");
+			}
+		}
+    }
+}
